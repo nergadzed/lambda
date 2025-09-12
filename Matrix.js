e655423e17a6e8f3c0ea8e3fa5f6d8c2793c5792
Davit Nergadze <nergadzed@gmail.com>
2025-09-12 18:39:05 +0400

    Predicate and Transform signature change

--- Matrix.js
+++ Matrix.js
@@ -3,12 +3,17 @@ const Equality = (target) => (value) => Object.is(value, target);
 class Matrix {
     y;
     x;
+    static traversal = {
+        abscissa: (magnitude, offset) => (entry, index) => Array.of(...Array(magnitude[1] - 1).fill([0, 1]), [1, 1 - magnitude[1]]),
+        ordinate: (magnitude, offset) => (entry, index) => Array.of(...Array(magnitude[0] - 1).fill([1, 0]), [1 - magnitude[0], 1]),
+    };
     static factory(source) {
         switch (true) {
             case Matrix.isFunc(source): return source;
             case Matrix.isSelf(source): return (y, x) => source.state[y]?.[x];
             case Matrix.isDeep(source): return (y, x) => source[y]?.[x];
             case Matrix.isFlat(source): return (source => (y, x) => source.next([y, x]).value)(source.values());
+            case Matrix.isIter(source): return (y, x) => source.next([y, x]).value;
             default: throw new TypeError;
         }
     }
@@ -24,6 +29,9 @@ class Matrix {
     static isFlat(target) {
         return Array.isArray(target);
     }
+    static isIter(target) {
+        return target instanceof Iterator;
+    }
     static rotate(target, n) {
         switch ((n % 4 + 4) % 4) {
             case 1: return new Matrix(target.x, target.y, target.cols.map(row => row.reverse()));
@@ -36,14 +44,7 @@ class Matrix {
     static transpose(target) {
         return new Matrix(target.x, target.y, target.cols);
     }
-    static traverse(target, ...walk) {
-        const init = walk.map(traversal => traversal.shift());
-        const move = walk.map(traversal => Array.from(Array(target.x), (_, index) => traversal[index % traversal.length]));
-        const source = init.map((initial, index) => {
-            const mGen = target[Symbol.iterator](initial, false);
-            return move[index].map(m => mGen.next(m).value?.[0]);
-        });
-        return new Matrix(walk.length, target.x, source);
+    static traverse(target) {
     }
     static frame(target, yStart, xStart, yEnd, xEnd) {
         const factory = Matrix.factory(target.rows);
@@ -67,26 +68,26 @@ class Matrix {
         const factory = Matrix.factory(target);
         let [Y, X] = [0, 0];
         const [Ys, Xs] = [
-            target.state.map((row, y) => row.every((entry, x) => predicate(entry, y, x)) ? Y++ : y), // Performance gain by direct access of the state rather than target.rows use?
-            target.transposed.map((col, x) => col.every((entry, y) => predicate(entry, y, x)) ? X++ : x), // Performance gain by direct access of the state rather than target.transposed use?
+            target.state.map((row, y) => row.every((entry, x) => predicate(entry, [y, x])) ? Y++ : y), // Performance gain by direct access of the state rather than target.rows use?
+            target.transposed.map((col, x) => col.every((entry, y) => predicate(entry, [y, x])) ? X++ : x), // Performance gain by direct access of the state rather than target.transposed use?
         ];
         return new Matrix(target.y - Y, target.x - X, (y, x) => factory(Ys[y], Xs[x]));
     }
     static omit(target, discard, predicate = Equality(discard)) {
         const factory = Matrix.factory(target);
-        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, y, x) ? null : entry)(factory(y, x)));
+        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, [y, x]) ? null : entry)(factory(y, x)));
     }
     static replace(target, discard, by, predicate = Equality(discard), transform = () => by) {
         const factory = Matrix.factory(target);
-        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, y, x) ? transform(entry, y, x) : entry)(factory(y, x)));
+        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, [y, x]) ? transform(entry, [y, x]) : entry)(factory(y, x)));
     }
     static apply(target, transform) {
         const factory = Matrix.factory(target);
-        return new Matrix(target.y, target.x, (y, x) => (entry => transform(entry, y, x))(factory(y, x)));
+        return new Matrix(target.y, target.x, (y, x) => (entry => transform(entry, [y, x]))(factory(y, x)));
     }
     static extract(target, entry, predicate = Equality(entry)) {
         const factory = Matrix.factory(target);
-        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, y, x) ? entry : null)(factory(y, x)));
+        return new Matrix(target.y, target.x, (y, x) => (entry => predicate(entry, [y, x]) ? entry : null)(factory(y, x)));
     }
     state;
     transposed;
@@ -109,14 +110,8 @@ class Matrix {
     row(index) { return this.state.at(index)?.map(identity => identity); }
     get cols() { return this.transposed.map(identity => [...identity]); }
     col(index) { return this.transposed.at(index)?.map(identity => identity); }
-    *[Symbol.iterator](initial = [0, 0], limit = this.y * this.x, traverse = Array.of(...Array(this.x).fill([0, 1]), [1, 1 - this.x]), relative = true) {
-        let [λ, y, x] = [0, ...initial], input, shift, modular = (index, axis) => index % this[axis];
-        while (limit-- > 0) {
-            shift = traverse[λ++ % traverse.length];
-        }
-    }
 }
 let m0 = new Matrix(4, 8, (y, x) => `${y}x${x}`);
-let m0gen = m0[Symbol.iterator]([0, 0], false);
+// let m0gen = m0[ Symbol.iterator ]( [ 0, 0 ], false )
 // let t0 = Matrix.traverse( m0, [ [ 0, 0 ], [ 0, 1 ] ], [ [ 1, 0 ], [ 0, 1 ] ], [ [ 2, 7 ], [ 0, -1 ] ], [ [ 0, 0 ], [ 1, 1 ] ] )
 // let m0 = new Matrix( 4, 8, ( y, x ) => y * x )
--- Matrix.ts
+++ Matrix.ts
@@ -1,97 +1,98 @@
-type Nullable<T> = T | null | undefined
+type Nullable<T> = void | T | null | undefined
 type Index = [ y: number, x: number ]
 type Factory<T> = ( ..._:Index ) => Nullable<T>
-type Source<T> = Factory<T> | Matrix<T> | Array<Array<Nullable<T>>> | Array<Nullable<T>>
-type Predicate<T> = ( value: T, ..._:Index ) => boolean
-type Transform<T> = ( value: T, ..._:Index ) => T
-type MGen<T, E extends [ entry: Nullable<T> ] | [] = [ entry: Nullable<T> ]> = Generator<[ ...E, ...Index ], void, Nullable<Index>>
-type Traversal = [ initial: Index, move: Index, ...Index[] ]
+type Source<T> = Factory<T> | Matrix<T> | Array<Array<Nullable<T>>> | Array<Nullable<T>> | Iterator<Nullable<T>, Nullable<T>, Nullable<Index>>
+type Predicate<T> = ( value: T, index:Index ) => boolean
+type Transform<T> = ( value: T, index:Index ) => T
 const Equality = ( target: unknown ) => ( value: unknown ) => Object.is( value, target )
 
 class Matrix<T /* extends string | number | bigint | boolean | symbol | NonNullable<object> | Function */> {
-    static factory   <T> ( source: Source<T> ): Factory<T> {
+    static traversal = {
+        abscissa: <S> ( magnitude: Index, offset: Index ) => ( entry: S, index: Index ) =>
+            Array.of( ...Array( magnitude[ 1 ] - 1 ).fill( [ 0, 1 ] ), [ 1, 1 - magnitude[ 1 ] ] ) as [ Index, ...Index[] ],
+        ordinate: <S> ( magnitude: Index, offset: Index ) => ( entry: S, index: Index ) =>
+            Array.of( ...Array( magnitude[ 0 ] - 1 ).fill( [ 1, 0 ] ), [ 1 - magnitude[ 0 ], 1 ] ) as [ Index, ...Index[] ],
+    }
+    static factory   <S> ( source: Source<S> ): Factory<S> {
         switch ( true ) {
-        case Matrix.isFunc<T>( source ): return                                         source
-        case Matrix.isSelf<T>( source ): return             ( y: number, x: number ) => source.state[ y ]?.[ x ]
-        case Matrix.isDeep<T>( source ): return             ( y: number, x: number ) => source      [ y ]?.[ x ]
-        case Matrix.isFlat<T>( source ): return ( source => ( y: number, x: number ) => source.next( [ y, x ] ).value )( source.values() )
+        case Matrix.isFunc<S>( source ):           return                                         source
+        case Matrix.isSelf<S>( source ):           return             ( y: number, x: number ) => source.state[ y ]?.[ x ]
+        case Matrix.isDeep<Nullable<S>>( source ): return             ( y: number, x: number ) => source[ y ]?.[ x ]
+        case Matrix.isFlat<Nullable<S>>( source ): return ( source => ( y: number, x: number ) => source.next( [ y, x ] ).value )( source.values() )
+        case Matrix.isIter<Nullable<S>>( source ): return             ( y: number, x: number ) => source.next( [ y, x ] ).value
         default: throw new TypeError
         }
     }
-    static isFunc    <T> ( target: unknown ): target is Factory<T> {
+    static isFunc    <S> ( target: unknown ): target is Factory<S> {
         return typeof target === 'function'
     }
-    static isSelf    <T> ( target: unknown ): target is Matrix<T> {
+    static isSelf    <S> ( target: unknown ): target is Matrix<S> {
         return target instanceof Matrix
     }
-    static isDeep    <T> ( target: unknown ): target is T[][] {
+    static isDeep    <S> ( target: unknown ): target is S[][] {
         return Array.isArray( target ) && target.every( member => Array.isArray( member ) )
     }
-    static isFlat    <T> ( target: unknown ): target is T[] {
+    static isFlat    <S> ( target: unknown ): target is S[] {
         return Array.isArray( target )
     }
-    static rotate    <T> ( target: Matrix<T>, n: number ): Matrix<T> {
+    static isIter    <S> ( target: unknown ): target is Iterator<S, Nullable<S>, Nullable<Index>> {
+        return target instanceof Iterator
+    }
+    static rotate    <S> ( target: Matrix<S>, n: number ): Matrix<S> {
         switch ( (n % 4 + 4) % 4 ) {
-        case 1: return new Matrix<T>( target.x, target.y, target.cols.map( row => row.reverse() ) )
-        case 2: return new Matrix<T>( target.y, target.x, target.rows.reverse().map( col => col.reverse() ) )
-        case 3: return new Matrix<T>( target.x, target.y, target.cols.reverse() )
-        case 0: return new Matrix<T>( target.y, target.x, target.rows )
+        case 1: return new Matrix<S>( target.x, target.y, target.cols.map( row => row.reverse() ) )
+        case 2: return new Matrix<S>( target.y, target.x, target.rows.reverse().map( col => col.reverse() ) )
+        case 3: return new Matrix<S>( target.x, target.y, target.cols.reverse() )
+        case 0: return new Matrix<S>( target.y, target.x, target.rows )
         default: throw new Error
         }
     }
-    static transpose <T> ( target: Matrix<T> ): Matrix<T> {
-        return new Matrix<T>( target.x, target.y, target.cols )
+    static transpose <S> ( target: Matrix<S> ): Matrix<S> {
+        return new Matrix<S>( target.x, target.y, target.cols )
     }
-    static traverse  <T> ( target: Matrix<T>, ...walk: [ Traversal, ...Traversal[] ] ): Matrix<T> {
-        const init     = walk.map( traversal => traversal.shift() as Index )
-        const move     = walk.map( traversal => Array.from( Array( target.x ), ( _, index ) => traversal[ index % traversal.length ] ) )
-        const source   = init.map( ( initial, index ) => {
-            const mGen = target[ Symbol.iterator ]( initial, false )
-            return move[ index ].map( m => mGen.next( m ).value?.[ 0 ] )
-        } )
-        return new Matrix<T>( walk.length, target.x, source )
+    static traverse  <S> ( target: Matrix<S> ) {
     }
-    static frame     <T> ( target: Matrix<T>, yStart: number, xStart: number, yEnd: number, xEnd: number ): Matrix<T> {
-        const factory = Matrix.factory<T>( target.rows )
-        return new Matrix<T>( ...[ yEnd - yStart, xEnd - xStart ], ( y, x ) => factory( yStart + y, xStart + x ) )
+    static frame     <S> ( target: Matrix<S>, yStart: number, xStart: number, yEnd: number, xEnd: number ): Matrix<S> {
+        const factory = Matrix.factory<S>( target.rows )
+        return new Matrix<S>( ...[ yEnd - yStart, xEnd - xStart ], ( y, x ) => factory( yStart + y, xStart + x ) )
     }
-    static scale     <T> ( target: Matrix<T>, yBy: number, xBy: number, ...sources: Source<T>[] ): Matrix<T> {
-        return new Matrix<T>( target.y + yBy, target.x + xBy, ...sources )
+    static scale     <S> ( target: Matrix<S>, yBy: number, xBy: number, ...sources: Source<S>[] ): Matrix<S> {
+        return new Matrix<S>( target.y + yBy, target.x + xBy, ...sources )
     }
-    static shift     <T> ( target: Matrix<T>, yBy: number, xBy: number, ...sources: Source<T>[] ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y, target.x, ( y, x ) => factory( y - yBy, x - xBy ), ...sources )
+    static shift     <S> ( target: Matrix<S>, yBy: number, xBy: number, ...sources: Source<S>[] ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y, target.x, ( y, x ) => factory( y - yBy, x - xBy ), ...sources )
     }
-    static inflate   <T> ( target: Matrix<T>, ...sources: Source<T>[] ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y * 2 - 1, target.x * 2 - 1, ( y, x ) => factory( y % 2 ? -1 : y >> 1, x % 2 ? -1 : x >> 1 ), ...sources )
+    static inflate   <S> ( target: Matrix<S>, ...sources: Source<S>[] ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y * 2 - 1, target.x * 2 - 1, ( y, x ) => factory( y % 2 ? -1 : y >> 1, x % 2 ? -1 : x >> 1 ), ...sources )
     }
-    static fill      <T> ( target: Matrix<T>, ...sources: [ Source<T>, ...Source<T>[] ] ): Matrix<T> {
-        return new Matrix<T>( target.y, target.x, ...sources )
+    static fill      <S> ( target: Matrix<S>, ...sources: [ Source<S>, ...Source<S>[] ] ): Matrix<S> {
+        return new Matrix<S>( target.y, target.x, ...sources )
     }
-    static deflate   <T> ( target: Matrix<T>, discard: T, predicate: Predicate<Nullable<T>> = Equality( discard ) ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
+    static deflate   <S> ( target: Matrix<S>, discard: S, predicate: Predicate<Nullable<S>> = Equality( discard ) ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
         let   [ Y,  X  ] = [ 0, 0 ]
         const [ Ys, Xs ] = [
-            target.state     .map( ( row, y ) => row.every( ( entry, x ) => predicate( entry, y, x ) ) ? Y++ : y ), // Performance gain by direct access of the state rather than target.rows use?
-            target.transposed.map( ( col, x ) => col.every( ( entry, y ) => predicate( entry, y, x ) ) ? X++ : x ), // Performance gain by direct access of the state rather than target.transposed use?
-        ]; return new Matrix<T>( target.y - Y, target.x - X, ( y, x ) => factory( Ys[ y ], Xs[ x ] ) )
+            target.state     .map( ( row, y ) => row.every( ( entry, x ) => predicate( entry, [ y, x ] ) ) ? Y++ : y ), // Performance gain by direct access of the state rather than target.rows use?
+            target.transposed.map( ( col, x ) => col.every( ( entry, y ) => predicate( entry, [ y, x ] ) ) ? X++ : x ), // Performance gain by direct access of the state rather than target.transposed use?
+        ]; return new Matrix<S>( target.y - Y, target.x - X, ( y, x ) => factory( Ys[ y ], Xs[ x ] ) )
     }
-    static omit      <T> ( target: Matrix<T>, discard: T, predicate: Predicate<Nullable<T>> = Equality( discard ) ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, y, x ) ? null : entry )( factory( y, x ) ) )
+    static omit      <S> ( target: Matrix<S>, discard: S, predicate: Predicate<Nullable<S>> = Equality( discard ) ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, [ y, x ] ) ? null : entry )( factory( y, x ) ) )
     }
-    static replace   <T> ( target: Matrix<T>, discard: T, by: T, predicate: Predicate<Nullable<T>> = Equality( discard ), transform: Transform<Nullable<T>> = () => by ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, y, x ) ? transform( entry, y, x ) : entry )( factory( y, x ) ) )
+    static replace   <S> ( target: Matrix<S>, discard: S, by: S, predicate: Predicate<Nullable<S>> = Equality( discard ), transform: Transform<Nullable<S>> = () => by ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, [ y, x ] ) ? transform( entry, [ y, x ] ) : entry )( factory( y, x ) ) )
     }
-    static apply     <T> ( target: Matrix<T>, transform: Transform<Nullable<T>> ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y, target.x, ( y, x ) => ( entry => transform( entry, y, x ) )( factory( y, x ) ) )
+    static apply     <S> ( target: Matrix<S>, transform: Transform<Nullable<S>> ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y, target.x, ( y, x ) => ( entry => transform( entry, [ y, x ] ) )( factory( y, x ) ) )
     }
-    static extract   <T> ( target: Matrix<T>, entry: T, predicate: Predicate<Nullable<T>> = Equality( entry ) ): Matrix<T> {
-        const factory = Matrix.factory<T>( target )
-        return new Matrix<T>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, y, x ) ? entry : null )( factory( y, x ) ) )
+    static extract   <S> ( target: Matrix<S>, entry: S, predicate: Predicate<Nullable<S>> = Equality( entry ) ): Matrix<S> {
+        const factory = Matrix.factory<S>( target )
+        return new Matrix<S>( target.y, target.x, ( y, x ) => ( entry => predicate( entry, [ y, x ] ) ? entry : null )( factory( y, x ) ) )
     }
     state      : ReadonlyArray<ReadonlyArray<Nullable<T>>>
     transposed : ReadonlyArray<ReadonlyArray<Nullable<T>>>
@@ -113,47 +114,25 @@ class Matrix<T /* extends string | number | bigint | boolean | symbol | NonNulla
     get cols ()           { return this.transposed             .map( identity => [ ...identity ] ) }
     col ( index: number ) { return this.transposed.at( index )?.map( identity =>      identity   ) }
 
-    * [ Symbol.iterator ] (
-        initial   : Index                 = [ 0, 0 ],
-        limit     : number                = this.y * this.x - initial[ 0 ] * initial[ 1 ],
-        traversal : [ Index, ...Index[] ] = Array.of(
-            ...Array( this.x - initial[ 1 ] ).fill( [ 0, 1 ] ), [ 1, 1 - this.x ],
-            ...Array( this.x                ).fill( [ 0, 1 ] ), [ 1, 1 - this.x ],
-        ) as [ Index, ...Index[] ],
-        relative  : boolean               = true,
-    ): MGen<T> {
-        let [ λ, y, x ] = [ 0, ...initial ],
-            input: Nullable<Index>,
-            increment = relative
-                ? ( index: Index, shift: Index ): Index => [ index[ 0 ] + shift[ 0 ], index[ 1 ] + shift[ 1 ] ]
-                : (     _: Index, shift: Index ): Index => [              shift[ 0 ],              shift[ 1 ] ]
-        while ( limit > λ ++ ) {
-            input = yield [ this.state[ input?.[ 0 ] ?? y ]?.[ input?.[ 1 ] ?? x ], ...input ?? [ y, x ] ];
-            [ y, x ] = increment( [ y, x ], traversal[ λ % traversal.length ] )
-        }
-    }
-
-    // *[ Symbol.iterator ] ( [ y, x ]: Index = [ 0, 0 ], absolute: boolean = true, traverse ): MGen<T> {
-    //     let input: Nullable<Index>
-    //     const move = absolute
-    //         ? ( index: number, axis: 0 | 1 ) => ( input?.[ axis ] ?? index + axis ) - axis
-    //         : ( index: number, axis: 0 | 1 ) => index += ( input?.[ axis ] ?? 0 + axis ) - axis
-    //     for ( let row = y; row < this.y; row++ ) {
-    //     for ( let col = x; col < this.x; col++ ) {
-    //         input = yield [ this.state[ row ]?.[ col ], row, col ]
-    //         row   = move( row, 0 )
-    //         col   = move( col, 1 )
-    //     } x = 0 }
+    // * [ Symbol.iterator ] (
+    //     initial   : Index                 = [ 0, 0 ],
+    //     limit     : number                = this.y * this.x - initial[ 0 ] * initial[ 1 ],
+    //     traversal : [ Index, ...Index[] ] = Matrix.traversal.abscissa( [ this.y, this.x ], initial ),
+    //     relative  : boolean               = true,
+    // ): Generator<[ entry: Nullable<T>, Index ], void, Nullable<Index>> {
+    //     let [ λ, y, x ] = [ 0, ...initial ], input: Nullable<Index>, increment = relative ?
+    //         ( index: Index, shift: Index ) => [ index[ 0 ] + shift[ 0 ], index[ 1 ] + shift[ 1 ] ] :
+    //         (     _: Index, shift: Index ) => [              shift[ 0 ],              shift[ 1 ] ]
+    //     while ( limit > λ )
+    //         ( input = yield [ this.state[ input?.[ 0 ] ?? y ]?.[ input?.[ 1 ] ?? x ], ...input ?? [ y, x ] ] ) ??
+    //         ( [ y, x ] = increment( [ y, x ], traversal[ λ ++ % traversal.length ] ) )
     // }
-    // * indexOf ( entry: Nullable<T>, initial: Index = [ 0, 0 ], predicate: Predicate<Nullable<T>> = Equality( entry ) ): MGen<T,[]> {
-    //     let entries = this[ Symbol.iterator ]( initial ),
-    //         result  = entries.next()
-    //     while ( ! result.done ) {
-    //         let input: Nullable<Index>
-    //         if ( predicate( ...result.value ) )
-    //             input = yield [ result.value[ 1 ], result.value[ 2 ] ]
-    //         result = entries.next( input )
-    //     } entries.return()
+    // * indexOf (
+    //     entry     : Nullable<T>,
+    //     initial   : Index = [ 0, 0 ],
+    //     predicate : Predicate<Nullable<T>> = Equality( entry )
+    // ): Generator<[ entry: Nullable<T>, Index ], void, void> {
+    //     yield * this[ Symbol.iterator ]( initial ).filter( entry => predicate( ...entry ) )
     // }
 }
 
